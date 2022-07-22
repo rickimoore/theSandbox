f9f303c2e961d6e284a4de399c51e2e9045989b2
Ricki Moore <mrricki.m.usmc@gmail.com>
2022-07-22 06:23:04 +0100

    fix: refactored apollo query for react-apllo-hook suspense feature

--- frontend/src/hooks/subgraph/useFetchAuctionQuery.js
+++ frontend/src/hooks/subgraph/useFetchAuctionQuery.js
@@ -1,29 +1,19 @@
-import {useQuery} from '@apollo/client';
+import {useQuery} from 'react-apollo-hooks';
 import {GET_AUCTION} from '../../apollo/queries/getAuction';
-import {useEffect, useState} from 'react';
 import numberToHex from '../../utils/numberToHex';
 
 const useFetchAuctionQuery = (auctionId) => {
-  const [auction, setAuction] = useState();
-  const { error, refetch } = useQuery(
+  const { data, error } = useQuery(
       GET_AUCTION,
       {
-        skip: true
-      }
+        skip: !auctionId,
+        suspend: true,
+        variables: {id: numberToHex(auctionId)},
+      },
   );
 
-  useEffect(() => {
-    if(auctionId) {
-      // eslint-disable-next-line no-unused-expressions
-      refetch({ id: numberToHex(auctionId) })?.then((response) => {
-        setAuction(response?.data.blockAuction);
-      });
-    }
-  }, [auctionId]);
-
-
   return {
-    data: auction,
+    data: data?.blockAuction,
     error
   }
 }
